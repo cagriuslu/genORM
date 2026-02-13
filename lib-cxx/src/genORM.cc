aae5534692d9b44d8dd2a61b83cf8c62c7a5bd26
Çağrı Uslu <uslu.cagri@gmail.com>
2026-02-13 19:48:50 +0100

    Add external dependencies

---
+++ lib-cxx/src/genORM.cc
@@ -0,0 +1,8 @@
+#include <genORM/genORM.h>
+#include <cstdio>
+
+using namespace genORM;
+
+void genORM::CreateSqliteDb() {
+	printf("Hello\n");
+}
---
+++ test-cxx/test.cc
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(Dummy, dummy) {
+	printf("Hello\n");
+}
